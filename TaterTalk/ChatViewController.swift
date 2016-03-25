3559834406eefa4b09786f691b796f6760071151
Doug Harper <doug@zewkey.com>
2016-03-24 21:28:35 -0500

    Add TimeStamp  to Model
    Add date to message class
    Add time stamp to the messages
    Update Messages Array
    Create AddMesssage Method
    Create GetMessages Method
    Update UITableViewDataSource
    Update UITableView instance
    Add ViewForHeaderInSection Method
    Set up header constraints
    Setup Formatter for header
    Add Footer spacing
    Update Scrolling to do sections

--- TaterTalk/ChatViewController.swift
+++ TaterTalk/ChatViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 
 class ChatViewController: UIViewController {
     
-    private let tableView = UITableView()
+    private let tableView = UITableView(frame: CGRectZero, style: .Grouped)
     
-    private var messages = [Message]()
+    private var sections = [NSDate: [Message]]()
+    private var dates = [NSDate]()
     private var bottomConstraint: NSLayoutConstraint!
     private let cellIdentifier = "Cell"
     private let newMessageField = UITextView()
@@ -31,19 +32,26 @@ class ChatViewController: UIViewController {
         // set up some fake data
         
         var localIncoming = true
+        var date = NSDate(timeIntervalSince1970: 1100000000)
         
         for i in 0...10 {           // loop thru 10x
             let m = Message()       // m = an instance of Message
             //m.text = String(i)      // m.text = i(count increment) as a string
             m.text = "This is a longer message."
+            m.timeStamp = date
             m.incoming = localIncoming
             localIncoming = !localIncoming
-            messages.append(m)      // add m to messages array
+            addMessage(m)      // add m to messages array
+            
+            // the modulo symbol to determine if the remainder is equal to 0. So every other value of i (odds and evens) will cause the if statement to evaluate to true. We then update the date variable so it will be the next day.
+            if i%2 == 0 {
+                date = NSDate(timeInterval: 60 * 60 * 24, sinceDate: date)
+            }
         }
         
-        for eachMessage in messages {
-            print(eachMessage, ":", eachMessage.text)
-        }
+//        for eachMessage in messages {
+//            print(eachMessage, ":", eachMessage.text)
+//        }
         
         // create new message area
         let newMessageArea = UIView()
@@ -151,29 +159,99 @@ class ChatViewController: UIViewController {
         let message = Message()
         message.text = text
         message.incoming = false
-        messages.append(message)
+        message.timeStamp = NSDate()
+        addMessage(message)
         newMessageField.text = ""
         tableView.reloadData()
         tableView.scrollToBottom()
         view.endEditing(true)
     }
     
+    func addMessage (message: Message) {
+        guard let date = message.timeStamp else { return }
+        let calendar = NSCalendar.currentCalendar()
+        let startDay = calendar.startOfDayForDate(date)
+        
+        var messages = sections[startDay]
+        if messages == nil {
+            dates.append(startDay)
+            messages = [Message]()
+        }
+        messages?.append(message)
+        sections[startDay] = messages
+    }
+    
 }
 
 
 extension ChatViewController: UITableViewDataSource {
+    
+    func getMessages(section: Int) -> [Message] {
+        let date = dates[section]
+        return sections[date]!
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return dates.count
+    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return messages.count
+        return getMessages(section).count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! ChatCell
+        let messages = getMessages(indexPath.section)
         let message = messages[indexPath.row]
+        
         cell.messageLabel.text = message.text
         cell.incoming(message.incoming)
-    // remove cell separator line
+    // remove cell separator line and set background color
+        cell.backgroundColor = UIColor.clearColor()
         cell.separatorInset = UIEdgeInsetsMake(0, tableView.bounds.size.width, 0, 0)
         return cell
+    }
+    
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = UIColor.clearColor()
+        let paddingView = UIView()
+        view.addSubview(paddingView)
+        paddingView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let dateLabel = UILabel()
+        paddingView.addSubview(dateLabel)
+        dateLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let constraints: [NSLayoutConstraint] = [
+            paddingView.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor),
+            paddingView.centerYAnchor.constraintEqualToAnchor(view.centerYAnchor),
+            dateLabel.centerXAnchor.constraintEqualToAnchor(paddingView.centerXAnchor),
+            dateLabel.centerYAnchor.constraintEqualToAnchor(paddingView.centerYAnchor),
+            paddingView.heightAnchor.constraintEqualToAnchor(dateLabel.heightAnchor, constant: 5),
+            paddingView.widthAnchor.constraintEqualToAnchor(dateLabel.widthAnchor, constant: 10),
+            view.heightAnchor.constraintEqualToAnchor(paddingView.heightAnchor)
+        ]
+        
+        NSLayoutConstraint.activateConstraints(constraints)
+        
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "MMM dd, YYYY"
+        dateLabel.text = formatter.stringFromDate(dates[section])
+        
+        paddingView.layer.cornerRadius = 10
+        paddingView.layer.masksToBounds = true
+        paddingView.backgroundColor = UIColor.greenColor()
+        
+        return view
+    }
+    
+    func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView()
+    }
+    
+    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0.01
     }
 }
 
